d6cefb3366c525d03c1c9de920ee642495eb33f7
Girish Vijay Dhole <girish.vijay@innovatechs.com>
2025-04-29 17:31:37 +0530

    Commit

--- src/main/java/com/example/student/entity/Gd_Subject.java
+++ src/main/java/com/example/student/entity/Gd_Subject.java
@@ -3,13 +3,11 @@ package com.example.student.entity;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
